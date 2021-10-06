af656c0035ec6daaec71bedebe613f3898155ec7
HumanValueStockExchange <hvse.info@protonmail.com>
2021-10-06 13:44:58 +0200

    about commit

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,7 +9,7 @@ const AboutPage = () => {
         <b>MAKING YOU BETTER</b>
         <p>What is the difference between each of us? Our value; This criterion seems to be subjective. therefore, none objective parameter exists yet to assess human value on an individual scale.The ability to find keys to success  is a vital component of human dignity and a fundamental human right. That’s why we stand for improve yourself without limits.</p>
         <b>SUCCESS WITHOUT LIMITS?</b>
-        <p>Succes is actually entraved by a oscietal issue: the social elevator elevator disapperance.Unlike other cyptocurrencies, HVSEcoin will not be a goal but a mean: the mean to your success and to improve yourself. In fact, HVSEcoin is the only way to access a new market: that of human values (Human value Stock Exchange).</p>
+        <p>Success is actually entraved by a societal issue: the social elevator elevator disapperance.Unlike other cyptocurrencies, HVSEcoin will not be a goal but a mean: the mean to your success and to improve yourself. In fact, HVSEcoin is the only way to access a new market: that of human values (Human Value Stock Exchange).</p>
         <b>OUR ROLE ABOUT THAT</b>
         <p>Our products make people better. <b>Individuals</b> can buy, use, and store them securely with our digital wallet and financial services. <b>Developers</b> get support building technologies that promote financial inclusivity. Everyone who visits HVSE.xyz can learn about the future of our system from a trusted source.</p>
         <b>JOIN US</b>
