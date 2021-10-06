77a2d45c7f49d7adffe213303eab09eee8fc8322
HumanValueStockExchange <hvse.info@protonmail.com>
2021-10-06 08:22:05 +0200

    First commit

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+// Step 1: Import React
+import * as React from 'react'
+import { Link } from 'gatsby'
+
+// Step 2: Define your component
+const AboutPage = () => {
+  return (
+    <main>
+      <title>About us</title>
+      <h1>About us</h1>
+      <Link to="/">Back to Home</Link>
+      <p>Our foundation allows you to bet on your success.</p>
+    </main>
+  )
+}
+
+// Step 3: Export your component
+export default AboutPage
