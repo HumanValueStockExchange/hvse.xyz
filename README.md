eca4d81c67299a9a052a9232761483dc167fc8c7
HumanValueStockExchange <hvse.info@protonmail.com>
2021-10-09 17:29:55 +0200

    footer initialization

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # HVSE [HVSE, Ͱ]
 
 [![Discord](https://img.shields.io/discord/714888181740339261?color=1C1CE1&label=hvse%20%7C%20Discord%20%F0%9F%91%8B%20&style=flat-square)](https://discord.gg/n4rRamkGuU)
-[![Twitter Follow](https://img.shields.io/twitter/follow/hvse.svg?style=social)](https://twitter.com/hvse)
+[![Twitter Follow](https://img.shields.io/twitter/follow/hvse.svg?style=social)](https://twitter.com/HVSE_Foundation)
  
 <h1 align="center" style="margin-top: 1em; margin-bottom: 3em;">
   <p><a href="https://hvse.xyz"><img alt="hvse logo" src="./logo.png" alt="hvse.xyz" width="125"></a></p>
@@ -15,7 +15,7 @@ This is the repo for the [hvse.xyz](https://hvse.xyz) website, a resource for th
 
 - [Whitepaper](?)
 
-- [Twitter](https://twitter.com/hvse)
+- [Twitter](https://twitter.com/HVSE_Foundation)
 
 - [Official subreddit](https://www.reddit.com/r/hvse/)
 
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,73 +1,24 @@
 import React from "react";
-import { MDBCol, MDBContainer, MDBRow, MDBFooter, MDBBtn, MDBIcon } from "mdbreact";
+import { Link } from "gatsby"
 import styles from "./footer.module.css";
 
-const Footer = () => {
-    return (
-        <MDBFooter className="font-small pt-0">
-            <MDBContainer>
-                <MDBRow className="pt-5 mb-3 text-center d-flex justify-content-center">
-                    <MDBCol md="2" className="b-3">
-                        <h6 className="title font-weight-bold">
-                            <a href="#!">About us</a>
-                        </h6>
-                    </MDBCol>
-                    <MDBCol md="2" className="b-3">
-                        <h6 className="title font-weight-bold">
-                            <a href="#!">Products</a>
-                        </h6>
-                    </MDBCol>
-                    <MDBCol md="2" className="b-3">
-                        <h6 className="title font-weight-bold">
-                            <a href="#!">Awards</a>
-                        </h6>
-                    </MDBCol>
-                    <MDBCol md="2" className="b-3">
-                        <h6 className="title font-weight-bold">
-                            <a href="#!">Help</a>
-                        </h6>
-                    </MDBCol>
-                    <MDBCol md="2" className="b-3">
-                        <h6 className="title font-weight-bold">
-                            <a href="#!">Contact</a>
-                        </h6>
-                    </MDBCol>
-                </MDBRow>
-                <hr className="rgba-white-light" style={{ margin: "0 15%" }} />
-                <MDBRow className="d-flex text-center justify-content-center mb-md-0 mb-4">
-                    <MDBCol md="8" sm="12" className="mt-5">
-                        <p style={{ lineHeight: "1.7rem" }}>
-                            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-                            quae ab illo inventore veritatis et quasi architecto beatae
-                            vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
-                            voluptas sit aspernatur aut odit aut fugit, sed quia
-                            consequuntur.
-            </p>
-                    </MDBCol>
-                </MDBRow>
-                <hr className="clearfix d-md-none rgba-white-light" style={{ margin: "10% 15% 5%" }} />
-                <MDBRow className="pb-3">
-                    <MDBCol md="12">
-                        <div className="mb-5 flex-center">
-                            <MDBIcon fab icon="twitter" size="lg" className="mx-2 white-text mr-md-4"></MDBIcon>
-                            <MDBIcon fab icon="reddit" size="lg" className="mx-2 white-text mr-md-4"></MDBIcon>
-                            <MDBIcon fab icon="discord" size="lg" className="mx-2 white-text mr-md-4"></MDBIcon>
-                            <MDBIcon fab icon="github" size="lg" className="mx-2 white-text mr-md-4"></MDBIcon>
-                            <MDBIcon fab icon="instagram" size="lg" className="mx-2 white-text mr-md-4"></MDBIcon>
-                            <MDBIcon fab icon="pinterest" size="lg" className="mx-2 white-text mr-md-4"></MDBIcon>
-                        </div>
-                    </MDBCol>
-                </MDBRow>
-            </MDBContainer>
-            <div className="footer-copyright text-center py-3">
-                <MDBContainer fluid>
-                    &copy; {new Date().getFullYear()} Copyright:
-                <a href="https://www.hvse.xyz" alt=""> HVSE Foundation </a>
-                </MDBContainer>
+const Footer = (props) => (
+    <div className={[footerStyles.footer, footerStyles.clearfix].join(" ")}>
+		<div className={footerStyles.footerCopy}>
+			<div className={footerStyles.menuFooterWrapper}>
+				<div>
+                    <ul className={footerStyles.menuFooter}>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/contact">Contact</Link></li>
+                        <li><Link to="/about">About</Link></li>
+                    </ul>
+                </div>
             </div>
-        </MDBFooter>
-    );
-}
+			<div className={footerStyles.clear}></div>
+			<p className={footerStyles.copyright}>© {(new Date().getFullYear())} <Link to="/">{props.title}</Link> | {props.tagline}</p>
+		</div>
+		<div className={footerStyles.socialIcons}></div>
+	</div>
+)
 
 export default Footer
