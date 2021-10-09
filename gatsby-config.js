34891c82c466a3e8109b810270a97ffaecbccb90
HumanValueStockExchange <hvse.info@protonmail.com>
2021-10-09 16:25:06 +0200

    architecture update

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,16 +16,5 @@ module.exports = {
       }
     },
     "gatsby-plugin-mdx",
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-material-design-for-bootstrap`,
-        short_name: `MDB starter`,
-        start_url: `/`,
-        background_color: `#4B5EB8`,
-        theme_color: `#68008C`,
-        display: `standalone`,
-        icon: `${__dirname}/src/images/logos/logo.png`, // This path is relative to the root of the site.
-      },
   ],
 };
