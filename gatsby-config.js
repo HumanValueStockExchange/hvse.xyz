3577844b0ac6a1640ed1a58f3de0dca6e75a9bc1
HumanValueStockExchange <hvse.info@protonmail.com>
2021-10-15 17:48:50 +0200

    index modification

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,5 +16,19 @@ module.exports = {
       }
     },
     "gatsby-plugin-mdx",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Human Value Stock Exchange",
+        short_name: "HVSE",
+        start_url: "/",
+        background_color: "white",
+        theme_color: "white",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/logos/logo.png" // This path is relative to the root of the site.
+      },
+    }
   ],
 };
