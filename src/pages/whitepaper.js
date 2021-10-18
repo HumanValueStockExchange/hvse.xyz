b1e7f0e536cf36320060be3313ec07832446165a
HumanValueStockExchange <hvse.info@protonmail.com>
2021-10-18 15:05:41 +0200

    whitepaper modification

--- src/pages/whitepaper.js
+++ src/pages/whitepaper.js
@@ -14,8 +14,8 @@ const WhitepaperPage = () => {
     <Layout pageTitle="Whitepaper">
 
     <Helmet>
-      <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
-      <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js">
+      <script src="//polyfill.io/v3/polyfill.min.js?features=es6"></script>
+      <script id="MathJax-script" async src="//cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js">
   </script>
     </Helmet>
 
